d01e9420a00cf4dd74fa1901d3f1181e2d73b0b7
ahmedtajdev <ahmedtaj.dev@gmail.com>
2026-07-26 02:48:24 +0300

    complete

--- CRUD_Using_HTTP/server.js
+++ CRUD_Using_HTTP/server.js
@@ -12,58 +12,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-// const server = http.createServer((req, res) => {
-//   if (req.method === "POST" && req.url === "/user") {
-//     let body = "";
-
-//     req.on("data", (chunk) => {
-//       body += chunk;
-//     });
-
-//     req.on("end", () => {
-//       try {
-//         const newUser = JSON.parse(body);
-
-//         const users = readUsers();
-
-//         const emailExists = users.some((user) => user.email === newUser.email);
-
-//         if (emailExists) {
-//           res.writeHead(400, { "content-Type": "application/json" });
-//           return res.end(JSON.stringify({ message: "Email already exists." }));
-//         }
-
-//         newUser.id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
-
-//         users.push(newUser);
-
-//         writeUsers(users);
-
-//         res.writeHead(201, { "content-Type": "application/json" });
-//         res.end(
-//           JSON.stringify({
-//             message: "User added successfully.",
-//             user: newUser,
-//           }),
-//         );
-//       } catch (err) {
-//         console.error(err);
-//         res.writeHead(400, { "content-Type": "application/json" });
-//         res.end(JSON.stringify({ message: err.message }));
-//       }
-//     });
-//   } else if (req.method === "PATCH" && req.url === "/user/id") {
-//     let body = "";
-
-//     req.on("data", (chunk) => {
-//       body += chunk;
-//     });
-//   } else {
-//     res.writeHead(404, { "content-Type": "application/json" });
-//     res.end(JSON.stringify({ message: "Route not found." }));
-//   }
-// });
-
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
